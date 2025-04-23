b132000802cb1e50c71818763d700a450785361b
SahanRansika <sahanransika07@gmail.com>
2025-04-23 23:49:17 +0530

    REGISTRATION LOADTABLE() & SAVE & UPDATE & DELETE & GENERATENEWID() SUCCESSFULLTY...!

--- src/main/java/healthcarecenter/dao/impl/RegistrationDAOImpl.java
+++ src/main/java/healthcarecenter/dao/impl/RegistrationDAOImpl.java
@@ -2,16 +2,18 @@ package healthcarecenter.dao.impl;
 
 import healthcarecenter.config.FactoryConfiguration;
 import healthcarecenter.dao.CrudDAO;
+import healthcarecenter.dao.RegistrationDAO;
 import healthcarecenter.entity.Program;
 import healthcarecenter.entity.Registration;
 import javafx.scene.control.Alert;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class RegistrationDAOImpl implements CrudDAO<Registration> {
+public class RegistrationDAOImpl implements RegistrationDAO {
     @Override
     public boolean save(Registration entity) throws SQLException,ClassNotFoundException{
         Session session = FactoryConfiguration.getInstance().getSession();
@@ -41,21 +43,90 @@ public class RegistrationDAOImpl implements CrudDAO<Registration> {
 
     @Override
 public boolean update(Registration entity) throws SQLException,ClassNotFoundException{
-        return false;
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            session.merge(entity);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 
     @Override
 public boolean delete(String id) throws SQLException,ClassNotFoundException{
-        return false;
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            Registration registration = session.get(Registration.class, id);
+            if (registration != null) {
+                session.remove(registration);
+                transaction.commit();
+                return true;
+            } else {
+                new Alert(Alert.AlertType.ERROR, "REGISTRATION NOT FOUND").show();
+                return false;
+            }
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 
     @Override
 public String generateNewId() throws SQLException,ClassNotFoundException{
-        return "";
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            Query<String> query = session.createQuery(
+                    "SELECT r.registrationId FROM Registration r ORDER BY r.registrationId DESC", String.class
+            );
+            query.setMaxResults(1);
+            String lastId = query.uniqueResult();
+
+            transaction.commit();
+
+            if (lastId != null) {
+                int lastNum = Integer.parseInt(lastId.replace("R", ""));
+                int nextNum = lastNum + 1;
+                return String.format("R%03d", nextNum);
+            } else {
+                return "R001";
+            }
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new RuntimeException("NO REGISTRATION ID", e);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public ArrayList<Registration> getAll() throws SQLException, ClassNotFoundException {
-        return null;
+        Session session = FactoryConfiguration.getInstance().getSession();
+        try {
+            return (ArrayList<Registration>) session.createQuery("FROM Registration ", Registration.class).list();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 }
